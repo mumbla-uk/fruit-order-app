60f78f71836d92de4cfa1942e13696bc2a6f5765
mumbla-uk <rossandrewjordan@icloud.com>
2025-06-05 00:55:47 +0100

    Successfully initialized Tailwind CSS and rebuilt for deployment

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // <--- THIS LINE IS CRUCIAL FOR SCANNING REACT FILES
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
